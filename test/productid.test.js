ea2f25c66589271af15d2d762733c6b7da153250
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2024-07-17 18:04:54 -0600

    to.unitTest_01

---
+++ test/productid.test.js
@@ -0,0 +1,46 @@
+import * as chai from "chai";
+import chaiHttp from "chai-http";
+import mongoose from "mongoose";
+import app from "../src/app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { assert } = chai;
+chai.use(chaiHttp); // Esto extiende chai con chaiHttp
+
+const uri = process.env.MONGODB_URI;
+const logger = console; // Uso de console como logger para simplificar
+
+const connection = async () => {
+  try {
+    await mongoose.connect(uri);
+    logger.info("Conexión a la base de datos exitosa");
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+};
+
+describe("test ProductsRouter", function () {
+  before(async function () {
+    await connection();
+  });
+
+  after(async function () {
+    await mongoose.disconnect();
+  });
+
+  it('get("/:productid") debe de retornar un producto por su id', function (done) {
+    const productId = "661f8219a5b7e2f6dbf013ef";
+    chai
+      .request(app)
+      .get(`/api/products/${productId}`)
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, "_id");
+        assert.equal(res.body._id, productId);
+        done();
+      });
+  });
+});
